6ed8aff54e7e0c662d47745fb4507de401bdce1b
ilya <ilie2004@yandex.ru>
2025-10-05 12:39:07 +0400

    fix: removed runserver

--- tests.py
+++ tests.py
@@ -1,8 +1,15 @@
-import requests
+from django.test import TestCase
+from django.urls import reverse
 
-
-resp = requests.get("http://0.0.0.0:8181/ping/")
-
-assert resp.text == '<html>\n    status: ok\n</html>'
-
-assert 200 <= resp.status_code < 300
+class BasicTests(TestCase):
+    def test_healthcheck(self):
+        response = self.client.get('/ping/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_settings_loaded(self):
+        from django.conf import settings
+        self.assertTrue(settings.DEBUG is not None)
+    
+    def test_database_config(self):
+        from django.conf import settings
+        self.assertEqual(settings.DATABASES['default']['ENGINE'], 'django.db.backends.postgresql')
